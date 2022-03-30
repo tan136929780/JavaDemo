0ccf1ffd8461eb4b1fec348a2f98549d1860d669
tanxianchen <tanxianchen@58.com>
2022-03-30 11:17:09 +0800

    定时任务

---
+++ src/main/java/test/demo/task/scheduled/ScheduledTask.java
@@ -0,0 +1,21 @@
+//package test.demo.task.scheduled;
+//
+//import org.springframework.scheduling.annotation.Async;
+//import org.springframework.scheduling.annotation.EnableAsync;
+//import org.springframework.scheduling.annotation.EnableScheduling;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
+//
+//@Component
+//@EnableScheduling
+//@EnableAsync
+//public class ScheduledTask {
+//    @Async
+//    @Scheduled(cron = "*/5 * * * * *")
+//    public void first() throws InterruptedException {
+//        System.out.println((new SimpleDateFormat("yyyy-MM-dd hh:mm:ss")).format(new Date()));
+//    }
+//}
