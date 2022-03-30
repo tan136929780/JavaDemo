ac0281ac521730601a039c2752456a427fe090d5
tanxianchen <tanxianchen@58.com>
2022-03-30 14:11:21 +0800

    定时任务

--- src/main/java/test/demo/task/schedule/ScheduledTask.java
+++ src/main/java/test/demo/task/schedule/ScheduledTask.java
@@ -15,6 +15,7 @@
 //public class ScheduledTask {
 //    @Async
 //    @Scheduled(cron = "*/5 * * * * *")
+//    @Scheduled(fixedDelay = 1000)
 //    public void first() throws InterruptedException {
 //        System.out.println((new SimpleDateFormat("yyyy-MM-dd hh:mm:ss")).format(new Date()));
 //    }
