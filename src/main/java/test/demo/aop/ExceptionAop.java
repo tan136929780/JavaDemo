1410956fb59752d5d4ea572f6294d3bafde7a131
tanxc1 <tanxc1@lenovo.com>
2022-05-07 17:27:13 +0800

    配置变更

--- src/main/java/test/demo/aop/ExceptionAop.java
+++ src/main/java/test/demo/aop/ExceptionAop.java
@@ -5,8 +5,6 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import test.demo.model.ReturnResult;
 import test.demo.utils.ResponseUtil;
