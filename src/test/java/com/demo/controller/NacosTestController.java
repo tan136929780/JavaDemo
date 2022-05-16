bd8874c88e1883ae62de987cd3c1fc96b4a6342f
tanxc1 <tanxc1@lenovo.com>
2022-05-16 22:15:16 +0800

    modify

--- src/test/java/com/demo/controller/NacosTestController.java
+++ src/test/java/com/demo/controller/NacosTestController.java
@@ -1,6 +1,9 @@
 package com.demo.controller;
 
-import com.demo.component.NacosComponent;
+import com.alibaba.nacos.api.annotation.NacosInjected;
+import com.alibaba.nacos.api.config.ConfigService;
+import com.alibaba.nacos.api.exception.NacosException;
+import com.demo.component.NacosConfigComponent;
 import com.google.protobuf.InvalidProtocolBufferException;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,12 +13,18 @@ import javax.annotation.Resource;
 @SpringBootTest
 public class NacosTestController {
     @Resource
-    NacosComponent nacosComponent;
+    NacosConfigComponent nacosConfigComponent;
+
+//    @NacosInjected
+//    ConfigService configService;
 
     @Test
-    public void nacosConfiguration() throws InvalidProtocolBufferException {
-        System.out.println(nacosComponent.getEnv());
-        System.out.println(nacosComponent.getServiceIp());
-        System.out.println(nacosComponent.getServicePort());
+    public void nacosConfiguration() throws InvalidProtocolBufferException, NacosException {
+        // 发布配置
+//        String content = "env=1111";
+//        configService.publishConfig("java", "DEFAULT_GROUP", content);
+        System.out.println(nacosConfigComponent.getEnv());
+        System.out.println(nacosConfigComponent.getServiceIp());
+        System.out.println(nacosConfigComponent.getServicePort());
     }
 }
