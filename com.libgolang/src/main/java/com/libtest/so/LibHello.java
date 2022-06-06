b23f085a9f882208a462af170afd8e4b471cc6a4
tanxc1 <tanxc1@lenovo.com>
2022-06-06 14:32:13 +0800

    java调用golang动态库

--- com.libgolang/src/main/java/com/libtest/so/LibHello.java
+++ com.libgolang/src/main/java/com/libtest/so/LibHello.java
@@ -5,7 +5,7 @@ import com.sun.jna.Library;
 import com.sun.jna.Native;
 
 public interface LibHello extends Library {
-    LibHello INSTANCE = Native.loadLibrary("/home/tan/www/go/src/lib/libhello.so", LibHello.class);
+    LibHello INSTANCE = Native.loadLibrary("/home/tan/www/go/src/goService/golib/libhello.so", LibHello.class);
 
     GoString.ByValue Hello(GoString.ByValue call);
 }
