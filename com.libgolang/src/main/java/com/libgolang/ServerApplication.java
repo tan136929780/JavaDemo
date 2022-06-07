e974a715f30dde278a84870392a3c08e4beb4d32
tanxc1 <tanxc1@lenovo.com>
2022-06-07 09:00:01 +0800

    优化 依赖引入

--- com.libgolang/src/main/java/com/libgolang/ServerApplication.java
+++ com.libgolang/src/main/java/com/libgolang/ServerApplication.java
@@ -2,10 +2,8 @@ package com.libgolang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
-@EnableTransactionManagement
 public class ServerApplication {
 
     public static void main(String[] args) {
