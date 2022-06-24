245426886bb166a67c6a97b91983846ba067daac
tanxianchen <136929780@qq.com>
2022-06-24 19:44:54 +0800

    vfile

--- com.client/src/test/java/com/client/grpc/GrpcTest.java
+++ com.client/src/test/java/com/client/grpc/GrpcTest.java
@@ -1,11 +1,18 @@
 package com.client.grpc;
 
 import com.alibaba.nacos.api.exception.NacosException;
+import com.alibaba.nacos.api.naming.pojo.Instance;
+import com.client.contract.hello.HelloGrpc;
+import com.client.contract.hello.HelloRequest;
 import com.client.contract.hello.HelloResponse;
 import com.client.service.HelloService;
+import io.grpc.Channel;
+import io.grpc.ManagedChannelBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import vfile.FileServiceGrpc;
+import vfile.Vfile;
 
 @SpringBootTest
 public class GrpcTest {
@@ -16,5 +23,13 @@ public class GrpcTest {
     public void test() throws NacosException {
         HelloResponse helloResponse = helloService.hello("Lyn");
         System.out.println(helloResponse);
+    }
+
+    @org.junit.Test
+    public void testVfile() {
+        Channel channel = ManagedChannelBuilder.forAddress("localhost", 8200).usePlaintext().build();
+        FileServiceGrpc.FileServiceBlockingStub fileServiceBlockingStub = FileServiceGrpc.newBlockingStub(channel);
+        Vfile.ServiceInfoRequest serviceInfoRequest = Vfile.ServiceInfoRequest.newBuilder().build();
+        System.out.println(fileServiceBlockingStub.serviceInfo(serviceInfoRequest));
     }
 }
